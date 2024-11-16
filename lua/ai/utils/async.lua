17b7186c6d3b6c2cca38ad15bd783e075720fb40
Tobias Walle <tobias.walle@maibornwolff.de>
2024-11-16 19:23:28 +0100

    update async utils

--- lua/ai/utils/async.lua
+++ lua/ai/utils/async.lua
@@ -1,5 +1,8 @@
 local co = coroutine
 
+--- Just mark a type as async. Due to limitations to the lua language server this is not fully typesafe.
+--- @alias Async<T> T
+
 --- Executes a function within a coroutine and handles its completion.
 --- @generic T
 --- @param func fun():T The function to execute within the coroutine.
@@ -30,80 +33,81 @@ local pong = function(func, callback)
   step()
 end
 
---- Wraps a function to create a thunk factory.
+--- Wraps a function to create a defer factory.
 --- @generic T
 --- @param func fun(...):T The function to wrap.
---- @return fun(...):fun(step?: fun(result:T)) -- A thunk factory.
+--- @return fun(...):fun(step?: fun(result:T)) -- A defer factory.
 local wrap_any = function(func)
   assert(type(func) == 'function', 'type error :: expected func')
   local factory = function(...)
     local params = { ... }
-    local thunk = function(step)
+    local defer = function(step)
       table.insert(params, step)
       return func(unpack(params))
     end
-    return thunk
+    return defer
   end
   return factory
 end
 
 --- @generic T
---- @param func fun( callback: fun(result: T)):unknown
---- @return fun(): fun(step?: fun(result:T))
+--- @param func fun(callback: fun(result: T))
+--- @return fun(): Async<T>
 local wrap_0 = function(func)
   return wrap_any(func)
 end
 
 --- @generic T, A
---- @param func fun(a: A, callback: fun(result: T)):unknown
---- @return fun(a: A): fun(step?: fun(result:T))
+--- @param func fun(a: A, callback: fun(result: T))
+--- @return fun(a: A): Async<T>
 local wrap_1 = function(func)
   return wrap_any(func)
 end
 
 --- @generic T, A, B
---- @param func fun(a: A, b: B, callback: fun(result: T)):unknown
---- @return fun(a: A, b: B):fun(step?: fun(result:T))
+--- @param func fun(a: A, b: B, callback: fun(result: T))
+--- @return fun(a: A, b: B): Async<T>
 local wrap_2 = function(func)
   return wrap_any(func)
 end
 
 --- @generic T, A, B, C
---- @param func fun(a: A, b: B, c: C, callback: fun(result: T)):unknown
---- @return fun(a: A, b: B, c: C):fun(step?: fun(result:T))
+--- @param func fun(a: A, b: B, c: C, callback: fun(result: T))
+--- @return fun(a: A, b: B, c: C): Async<T>
 local wrap_3 = function(func)
   return wrap_any(func)
 end
 
 --- @generic T, A, B, C, D
---- @param func fun(a: A, b: B, c: C, d: D, callback: fun(result: T)):unknown
---- @return fun(a: A, b: B, c: C, d: D):fun(step?: fun(result:T))
+--- @param func fun(a: A, b: B, c: C, d: D, callback: fun(result: T))
+--- @return fun(a: A, b: B, c: C, d: D): Async<T>
 local wrap_4 = function(func)
   return wrap_any(func)
 end
 
 --- @generic T, A, B, C, D, E
---- @param func fun(a: A, b: B, c: C, d: D, e: E, callback: fun(result: T)):unknown
---- @return fun(a: A, b: B, c: C, d: D, e: E):fun(step?: fun(result:T))
+--- @param func fun(a: A, b: B, c: C, d: D, e: E, callback: fun(result: T))
+--- @return fun(a: A, b: B, c: C, d: D, e: E): Async<T>
 local wrap_5 = function(func)
   return wrap_any(func)
 end
 
---- Joins multiple thunks into a single thunk.
+--- Joins multiple async values into a single one.
 --- @generic T
---- @param thunks fun(step:fun(result:T))[] The array of thunks to join.
---- @return fun(step:fun(results:T[])) A single thunk that represents the joined thunks.
-local join = function(thunks)
-  local len = #thunks
+--- @param async_values Async<T>[]
+--- @return Async<T[]>
+local join = function(async_values)
+  local len = #async_values
   local done = 0
   local acc = {}
 
-  local thunk = function(step)
+  --- @type any
+  local defer = function(step)
     if len == 0 then
       return step()
     end
-    for i, tk in ipairs(thunks) do
-      assert(type(tk) == 'function', 'thunk must be function')
+    for i, tk in ipairs(async_values) do
+      assert(type(tk) == 'function', 'defer must be function')
       local callback = function(...)
         acc[i] = { ... }
         done = done + 1
@@ -114,25 +118,25 @@ local join = function(thunks)
       tk(callback)
     end
   end
-  return thunk
+  return defer
 end
 
 --- Suspends the execution of the current coroutine, yielding control to the provided function.
 --- @generic T
---- @param defer fun(step?: fun(result: T))
---- @return T The result of the deferred function.
-local await = function(defer)
-  assert(type(defer) == 'function', 'type error :: expected func')
-  return co.yield(defer)
+--- @param async_value Async<T>
+--- @return T
+local await = function(async_value)
+  assert(type(async_value) == 'function', 'type error :: expected func')
+  return co.yield(async_value)
 end
 
 --- Suspends the execution of the current coroutine, yielding control to the provided array of functions.
 --- @generic T
---- @param defer (fun():T)[] The array of functions to defer.
+--- @param async_values Async<T>[] The array of functions to defer.
 --- @return T[] The result of the deferred functions.
-local await_all = function(defer)
-  assert(type(defer) == 'table', 'type error :: expected table')
-  return co.yield(join(defer))
+local await_all = function(async_values)
+  assert(type(async_values) == 'table', 'type error :: expected table')
+  return co.yield(join(async_values))
 end
 
 local async = wrap_1(pong)

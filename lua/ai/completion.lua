1a8b4e166454b70c5da0ae18a2da9253907c8295
Tobias Walle <tobias.walle@maibornwolff.de>
2024-11-30 13:15:08 +0100

    refactor completion

--- lua/ai/completion.lua
+++ lua/ai/completion.lua
@@ -1,8 +1,7 @@
 -- Define a module
 local M = {}
 
-local TOKEN_START = '<|COMPLETION_START|>'
-local TOKEN_END = '<|COMPLETION_END|>'
+local TOKEN = '<|CURSOR|>'
 
 local system_prompt = vim
   .trim([[
@@ -10,73 +9,115 @@ You are an intelligent code auto-completion assistant.
 Your task is to provide accurate and context-aware code suggestions based on the given file content and cursor position.
 
 INPUT:
-- You are getting some code as input with the cursor position marked as {{TOKEN_START}}{{TOKEN_END}}.
+- You are getting some code as input with the cursor position marked as {{TOKEN}}.
 - Additional instructions might be added above the cursor as comments. If this is the case, try to solve the task in your completion.
 
 OUTPUT:
-- Complete the code between {{TOKEN_START}}{{TOKEN_END}}.
-- ALWAYS wrap your completion with {{TOKEN_START}} and {{TOKEN_END}}. NEVER OMIT THESE MARKERS.
-- ONLY output your completion! NO SURROUNDING CODE!
-- If there is already an ident before {{TOKEN_START}}, do not repeat it!
+- Complete the code at {{TOKEN}}.
+- Repeat the context given to you before and after {{TOKEN}}.
+- Answer in a single code block.
 - Try to immitate the patterns and code style already existing in the file.
-
-Provide clean, well-documented code completions without additional explanations.
+- NEVER modify existing code, ONLY add new code
+- NEVER add comments in places where they don't make sense syntactically
 
 ## Example 1
 <input>
 ```javascript
 function helloWorld() {
-  {{TOKEN_START}}{{TOKEN_END}}
+  {{TOKEN}}
 }
 ```
+
+Start with EXACTLY:
+`function helloWorld() {
+  `
+End with EXACTLY:
+`
+}`
 </input>
 
 <output>
-  {{TOKEN_START}}console.log('Hello World'){{TOKEN_END}}
+```javascript
+function helloWorld() {
+  console.log('Hello World')
+}
+```
 </output>
 
 ## Example 2
 <input>
 ```javascript
-function add({{TOKEN_START}}{{TOKEN_END}}) {
+function add({{TOKEN}}) {
   return a + b;
 }
 ```
+
+Start with EXACTLY:
+`function add(`
+
+End with EXACTLY:
+`) {
+  return a + b;
+}`
 </input>
 
 <output>
-{{TOKEN_START}}a, b{{TOKEN_END}}
+```javascript
+function add(a, b) {
+  return a + b;
+}
+```
 </output>
 
 ## Example 3
 <input>
 ```lua
-{{TOKEN_START}}{{TOKEN_END}}
+{{TOKEN}}
 function say_hello(name)
   print("Hello " .. name)
 end
 ```
+
+Start with EXACTLY:
+``
+
+End with EXACTLY:
+`
+function say_hello(name)
+`
 </input>
 
 <output>
-{{TOKEN_START}}---Print "Hello <name>" to the console.
+```lua
+---Print "Hello <name>" to the console.
 ---@param name string
----@return nil{{TOKEN_END}}
+---@return nil
+function say_hello(name)
+```
 </output>
 
 ## Example 4
 <input>
 ```lua
 const firstName = "Karl"
-console.log(`Hello ${{{TOKEN_START}}{{TOKEN_END}}}`);
+console.log(`Hello ${{{TOKEN}}}`);
 ```
+
+Start with EXACTLY:
+`console.log(`Hello ${`
+
+End with EXACTLY:
+`}`);
+`
 </input>
 
 <output>
-{{TOKEN_START}}firstName{{TOKEN_END}}
+```lua
+console.log(`Hello ${firstName}`);
+```
 </output>
 ]])
-  :gsub('{{(.-)}}', { TOKEN_START = TOKEN_START, TOKEN_END = TOKEN_END })
+  :gsub('{{(.-)}}', { TOKEN = TOKEN })
 
 local ns_id = vim.api.nvim_create_namespace('ai_completion')
 
@@ -100,9 +141,17 @@ function M.trigger_completion()
 
   -- Insert the cursor marker at the current position
   local cursor_line = lines[row]
-  local before = cursor_line:sub(1, col)
-  local after = cursor_line:sub(col + 1)
-  lines[row] = before .. '<|START|><|END|>' .. after
+  local before_in_line = cursor_line:sub(1, col)
+  local after_in_line = cursor_line:sub(col + 1)
+  lines[row] = before_in_line .. TOKEN .. after_in_line
+
+  local lines_before = vim.list_slice(lines, row - 2, row - 1)
+  local lines_after = vim.list_slice(lines, row + 1, row + 2)
+  -- We ignore that is before and after in line, because this confuses the model
+  local context_before = vim.list_extend(lines_before, { before_in_line })
+  local context_after = vim.list_extend({ after_in_line }, lines_after)
+  local context_before_str = table.concat(context_before, '\n')
+  local context_after_str = table.concat(context_after, '\n')
 
   table.insert(lines, 1, '```' .. lang)
   table.insert(lines, '```')
@@ -110,7 +159,24 @@ function M.trigger_completion()
   -- Insert file name as well
   table.insert(lines, 1, 'FILE: ' .. vim.fn.expand('%'))
 
+  -- Make the task very clear
+  local function format_context(context)
+    table.insert(lines, '`' .. (context[1] or ''))
+    lines = vim.list_extend(lines, vim.list_slice(context, 2, #context - 1))
+    table.insert(lines, (context[#context] or '') .. '`')
+  end
+
+  table.insert(lines, '')
+  table.insert(lines, 'Start your completion EXACTLY with:')
+  format_context(context_before)
+  table.insert(lines, '')
+  table.insert(lines, 'And end EXACTLY with:')
+  format_context(context_after)
+
   local content = table.concat(lines, '\n')
+  print('REQUEST')
+  print(content)
+  print('REQUEST END')
 
   local cancelled = false
   local response_content = ''
@@ -139,33 +205,44 @@ function M.trigger_completion()
         return
       end
       response_content = update.response or ''
-      -- Extract content between tokens
-      suggestion = string.match(
-        response_content,
-        TOKEN_START .. '(.-)' .. TOKEN_END
-      ) or ''
-      -- If <|END|> is not found, extract content to the end of the line
-      if suggestion == '' then
-        suggestion = string.match(response_content, TOKEN_START .. '(.-)\n')
-          or ''
+      -- Remove code block
+      local code = require('ai.utils.treesitter').extract_code(response_content)
+      if code ~= nil then
+        code = code.sub(code, #context_before_str + 1)
+        code = code.sub(code, 1, #code - #after_in_line - #context_after_str)
+        -- Remove prefix
+        -- -- Extract content between tokens
+        -- suggestion = string.match(
+        --   response_content,
+        --   TOKEN_START .. '(.-)' .. TOKEN_END
+        -- ) or ''
+        -- -- If <|END|> is not found, extract content to the end of the line
+        -- if suggestion == '' then
+        --   suggestion = string.match(response_content, TOKEN_START .. '(.-)\n')
+        --     or ''
+        -- end
+        -- if suggestion == '' then
+        --   suggestion = string.match(response_content, '(.-)' .. TOKEN_END) or ''
+        -- end
+        suggestion = code
+        render_ghost_text(suggestion or '...')
       end
-      if suggestion == '' then
-        suggestion = string.match(response_content, '(.-)' .. TOKEN_END) or ''
-      end
-      render_ghost_text(suggestion or '...')
     end,
     on_exit = function()
-      if vim.trim(suggestion) == '' then
-        -- Remove code block if present
-        local fallback = require('ai.utils.treesitter').extract_code(
-          response_content
-        ) or response_content
-        -- Remove tokens if present
-        fallback = fallback:gsub(TOKEN_START, ''):gsub(TOKEN_END, '')
-        render_ghost_text(fallback)
-      else
-        render_ghost_text(suggestion)
-      end
+      print('RESPONSE')
+      print(response_content)
+      print('RESPONSE END')
+      -- if vim.trim(suggestion) == '' then
+      --   -- Remove code block if present
+      --   local fallback = require('ai.utils.treesitter').extract_code(
+      --     response_content
+      --   ) or response_content
+      --   -- Remove tokens if present
+      --   fallback = fallback:gsub(TOKEN_START, ''):gsub(TOKEN_END, '')
+      --   render_ghost_text(fallback)
+      -- else
+      --   render_ghost_text(suggestion)
+      -- end
     end,
   })
 
